055c4c7d90234f2ae7d25f000627373394a561fa
Franklyn de Quadros <franklynrtqu@gmail.com>
2021-12-10 09:58:33 -0300

    Inclusão de rotas

---
+++ src/Controller/Persistencia.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+use Doctrine\ORM\EntityManagerInterface;
+
+class Persistencia implements InterfaceControladorRequisicao
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct()
+    {
+        $this->entityManager = (new EntityManagerCreator())->getEntityManager();
+    }
+
+    public function processaRequisicao(): void
+    {
+        $descricao = filter_input(
+            INPUT_POST,
+            'descricao',
+            FILTER_SANITIZE_STRING
+        );
+        $curso = new Curso();
+        $curso->setDescricao($descricao);
+        $this->entityManager->persist($curso);
+        $this->entityManager->flush();
+
+        header('Location: /listar-cursos', true, 302);
+    }
+}
